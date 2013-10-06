0855f6b6d2a17e83f0ef1cee407cdb1be661fd00
Christine Luc <christine.luc@gmail.com>
2013-10-06 13:21:30 -0700

    Added Rails App

---
+++ Skillster2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillster2::Application.config.secret_token = '134a2a4e54a3f78ea17113fc4b8d31e4d4bb4eb5e96f03859e96fa812fa110c2a7798751f4d3f7dfc2c515ac06179d307da711707e0f6bc37b7f0fd5efa76ac0'
---
+++ skillster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillster::Application.config.secret_token = 'b5a464f4835e73e715cd9a8a91545b614a3a1ce6fdd6e8b905536dd636012a94e1d57a6dee4af27a0dcb1baefa9258808bf8dd6a44c1ac6ae5ee063bd08460dc'
